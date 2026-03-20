d3268f7caac968302d2a779e3cd23176c4f0f354
Argbin <Argbin05@gmail.com>
2026-03-20 11:45:04 +0100

    Add function insertX
    The purpose of the function is to insert a number X into a certain position in an existing c-array. Some of the elements in the
    c-array mut be shifted to the right to make space for X.
    
    The number X will be inserted on the pX adress. pBegin points towards the first element in the array, and pEnd points towards the
    first empty space in the array.
    
    pBegin <= pX <= pEnd is allways true according to the instructions.

--- 20250320-Exercises.cpp
+++ 20250320-Exercises.cpp
@@ -8,3 +8,16 @@ using namespace std ;
 void exercises20250320IN(){
 
 }
+
+/*
+Funktionens uppgift är att stoppa in ett tal x på en viss plats i en befintlig c-array. Vissa av
+arrayens befintliga element måste först skiftas ett steg åt höger för att skapa plats åt x.
+*/
+
+void insertX(float x, float* pX, float* pBegin, float* pEnd){
+    while(pEnd > pX){
+        *pEnd = *(pEnd-1);
+        pEnd -= 1;
+    }
+    *pEnd = x;
+}
